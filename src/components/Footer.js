33d10f8d335d29ca9c4cefd87aefaf6fc633eed2
gomdori2 <dbstjdghks21@naver.com>
2024-04-05 17:56:18 +0900

    feature : 헤더 1차 작업 진행 완료

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Footer = () => {
-    // 하단영역
-    return <div class="footer"></div>;
-}
+  // 하단영역
+  return <div class="footer"></div>;
+};
 
 export default Footer;
